3573298a77566ae21e3543fdd3323a8eca9e3e7b
botextreme <104403771+botextreme@users.noreply.github.com>
2022-06-01 17:47:46 -0500

    Update gp-dylux.js

--- plugins/gp-dylux.js
+++ plugins/gp-dylux.js
@@ -1,15 +1,20 @@
 let handler = async m => m.reply(`
 
-≡  *DyLux  ┃ ᴮᴼᵀ*   GRUPOS
+≡  *Scorpion  ┃ ᴮᴼᵀ*   GRUPOS
 
 ▢ Grupo WhatsApp 
- https://instabio.cc/fg98ff
+ https://chat.whatsapp.com/LN1dF710mLiJjEpzF1vq24
+ 
+ https://chat.whatsapp.com/JWHlCxaI9Ra25vGxuJqQ0s
+ 
+*nekosmic bot*
+https://chat.whatsapp.com/FR1N45RJOLh4BcAvUyssLk
 
 ▢ *Grupo Telegram*
- https://t.me/fgawgp
+ https://t.me/bot_scorpion0
 
 ▢ *YouTube*
-• https://www.youtube.com/fg98f
+• https://www.youtube.com/channel/UCPR7908-oVQzsdWy94enOJA/featured
 
 
 `.trim())
