e9064dc93e1538e5a86d536c0fd00a3e64232773
botextreme <104403771+botextreme@users.noreply.github.com>
2022-06-01 23:06:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('DyLux\nWhatsApp Bot', {
+say('Scorpion\nWhatsApp Bot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-say(`'${name}' By @fg98._`, {
+say(`'${name}' By @scorpion_bot06`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
